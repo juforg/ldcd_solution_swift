775ba9be944fca31127faf5d2f71be22af07f838
juforg <juforg@sina.com>
2016-04-27 00:16:57 +0800

    modify

--- BestTimetoBuyandSellStockII.playground/Contents.swift
+++ BestTimetoBuyandSellStockII.playground/Contents.swift
@@ -4,15 +4,15 @@ import UIKit
 
 var str = "Hello, playground"
 
-func rob(nums: [Int]) -> Int {
-    let len = nums.count;
+func maxProfit(prices: [Int]) -> Int {
+    let len = prices.count;
     var maxprofittmp = 0;
     if(len == 0) {return 0}
     for i in 1 ..< len{
-        let d_tmp=nums[i]-nums[i-1];
+        let d_tmp=prices[i]-prices[i-1];
         maxprofittmp += (d_tmp>0 ? d_tmp : 0);
     }
     return maxprofittmp;
 }
 
-rob([397,6621,4997,341]);
+maxProfit([397,6621,4997,341]);
